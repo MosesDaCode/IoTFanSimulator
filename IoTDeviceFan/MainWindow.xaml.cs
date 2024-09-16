12ef087721e87439b87fda45195f945b0eab1d93
hmacker <chammas_mousa@live.se>
2024-09-16 17:00:33 +0200

    Add project files.

---
+++ IoTDeviceFan/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using IoTDeviceFan.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IoTDeviceFan
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public FanViewModel ViewModel {  get; set; }
+		public MainWindow()
+		{
+			InitializeComponent();
+			ViewModel = new FanViewModel();
+			DataContext = ViewModel;
+		}
+
+		private void TopWindowBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			DragMove();
+		}
+		private void StartButton_Click(object sender, RoutedEventArgs e)
+		{
+			ViewModel.ToggleDevice();
+
+			var sb = (BeginStoryboard)TryFindResource("rotate-sb");
+
+			if (ViewModel.IsRunning)
+			{
+				sb.Storyboard.Begin();
+			}
+			else
+			{
+				sb.Storyboard.Stop();
+			}
+
+
+		}
+		private void ExitButton_Click(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		
+	}
+}
---
+++ IoTDeviceFan/ViewModels/FanViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTDeviceFan.ViewModels
+{
+    public class FanViewModel : INotifyPropertyChanged
+    {
+		private bool _isRunning;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public bool IsRunning
+		{
+			get { return _isRunning; }
+			set
+			{
+				_isRunning = value;
+				OnPropertyChanged(nameof(IsRunning));
+				OnPropertyChanged(nameof(ButtonText));
+			}
+		}
+
+		public string ButtonText => IsRunning ? "STOP" : "START";
+
+
+		public void ToggleDevice()
+		{
+			IsRunning = !IsRunning;
+		}
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
